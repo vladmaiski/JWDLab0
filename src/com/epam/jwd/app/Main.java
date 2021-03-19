b17b885836b8d668c5bff472ace2b1a5068dc42b
vladmaiski <cs_strim@mail.ru>
2021-03-19 13:13:04 +0300

    Add Main class and connect log4j

---
+++ src/com/epam/jwd/app/Main.java
@@ -0,0 +1,16 @@
+package com.epam.jwd.app;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
